689acba2726e935e399a7a05e49123fb0156f1c4
vanessa-maganhoto <82552260+vanessa-maganhoto@users.noreply.github.com>
2021-11-04 20:02:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let tituloForm = document.querySelector('#titulo-form')
+let descricaoTextArea = document.querySelector('#descricao')
+let linkImagem = document.querySelector('#link-imagem')
+let botao = document.querySelector('#botao')
+
+botao.addEventListener('click', criarCard)
+
+function criarCard(e){
+    e.preventDefault()
+
+    let h2 = document.createElement('h2')
+    h2.innerText = 'Veja seus cards'
+    document.querySelector('div').appendChild(h2)
+
+    let h3 = document.createElement('h3')
+    h3.innerText = tituloForm.value 
+    document.querySelector('div').appendChild(h3)
+
+    let p = document.createElement('p')
+    p.innerText = descricaoTextArea.value
+    document.querySelector('div').appendChild(p)
+
+    let img = document.createElement('img')
+    img.setAttribute('src', linkImagem.value)
+    document.querySelector('div').appendChild(img)
+    
+}
