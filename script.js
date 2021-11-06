48efbea6a241a8a9720b6c7d3f694258a65fd7f1
vanessa-maganhoto <82552260+vanessa-maganhoto@users.noreply.github.com>
2021-11-06 20:06:14 -0300

    Inputs
    Acréscimo dos botões relativos a região e a visitação

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-let tituloForm = document.querySelector('#titulo-form')
-let descricaoTextArea = document.querySelector('#descricao')
-let linkImagem = document.querySelector('#link-imagem')
-let botao = document.querySelector('#botao')
+const tituloForm = document.querySelector('#titulo-form')
+const descricaoTextArea = document.querySelector('#descricao')
+const linkImagem = document.querySelector('#link-imagem')
+const botao = document.querySelector('#botao')
+const regiao = document.getElementsByName('regiao')
+const visitacao = document.getElementById('visitacao')
 
 botao.addEventListener('click', criarCard)
 
-function criarCard(e){
+function criarCard(e) {
     e.preventDefault()
 
-    let h3 = document.createElement('h3')
-    h3.innerText = tituloForm.value 
-    document.querySelector('div').appendChild(h3)
+    const h3 = document.createElement('h3')
+    h3.innerText = tituloForm.value
+    document.querySelector('div.card').appendChild(h3)
 
-    let p = document.createElement('p')
-    p.innerText = descricaoTextArea.value
-    document.querySelector('div').appendChild(p)
+    const pDescricaoTextArea = document.createElement('p')
+    pDescricaoTextArea.innerText = descricaoTextArea.value
+    document.querySelector('div.card').appendChild(pDescricaoTextArea)
 
-    let img = document.createElement('img')
+    const img = document.createElement('img')
     img.setAttribute('src', linkImagem.value)
-    document.querySelector('div').appendChild(img)
+    document.querySelector('div.imagem').appendChild(img)
+
+    let regiaoMarcada
+    for (reg of regiao){
+        if (reg.checked){
+            regiaoMarcada = reg.value;
+        }
+    }
+
+    const pRegiaoMarcada = document.createElement('p')
+    pRegiaoMarcada.innerText = `O ponto turístico ${tituloForm.value} está localizado na região ${regiaoMarcada}`
+    document.querySelector('div.card').appendChild(pRegiaoMarcada)
+    
+    const pVisitacao = document.createElement('p')
+    pVisitacao.innerText = (visitacao.checked == true) 
+    ? `Você já conhece ${tituloForm.value}` 
+    : `Você ainda não visitou ${tituloForm.value}`
+    document.querySelector('div.card').appendChild(pVisitacao)
 
 }
